096274dccf28f16eaab95ab9013a41f21ce8d59f
29th-Day <gerald.franschitz@gmail.com>
2023-04-15 21:36:16 +0200

    procedural api

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
-#include "activations.h"
-#include "kernels.h"
-#include "states.h"
+#include "engine.h"
 #include "display.h"
 
-#include "engine.h"
-
-#define assert(x, msg) {if (!(x)) {fprintf(stderr, "Assertion error: %s", msg); exit(-2);}}
+#define assert(x, msg)                                   \
+    {                                                    \
+        if (!(x))                                        \
+        {                                                \
+            fprintf(stderr, "Assertion error: %s", msg); \
+            exit(-2);                                    \
+        }                                                \
+    }
 
 #define EQUAL_S(a, b) strcmp(a, b) == 0
 
-struct Arguments {
+struct Arguments
+{
     int height;
     int width;
     int scale;
@@ -22,22 +25,22 @@ struct Arguments {
     int seed;
 };
 
-void parseArgs(int argc, char** argv, Arguments *args)
+void parseArgs(int argc, char **argv, Arguments *args)
 {
     args->seed = (int)time(NULL);
 
-    for(int i = 1; i < argc; i+=2)
+    for (int i = 1; i < argc; i += 2)
     {
-        if(EQUAL_S(argv[i], "-h"))
-            args->height = atoi(argv[i+1]);
+        if (EQUAL_S(argv[i], "-h"))
+            args->height = atoi(argv[i + 1]);
         else if (EQUAL_S(argv[i], "-w"))
-            args->width = atoi(argv[i+1]);
+            args->width = atoi(argv[i + 1]);
         else if (EQUAL_S(argv[i], "-s"))
-            args->scale = atoi(argv[i+1]);
+            args->scale = atoi(argv[i + 1]);
         else if (EQUAL_S(argv[i], "-fps"))
-            args->fps = atoi(argv[i+1]);
+            args->fps = atoi(argv[i + 1]);
         else if (EQUAL_S(argv[i], "-seed"))
-            args->seed = atoi(argv[i+1]);
+            args->seed = atoi(argv[i + 1]);
     }
 
     assert(args->height > 0, "HEIGHT must be greater than 0");
@@ -46,32 +49,36 @@ void parseArgs(int argc, char** argv, Arguments *args)
     assert(args->fps > 0, "FPS must be greater than 0");
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     // SETUP
     Arguments args;
     parseArgs(argc, argv, &args);
 
-    srand(args.seed);
-    printf("seed: %i\n", args.seed);
-
     Display display = Display(args.height, args.width, args.scale, args.fps);
 
-    LifeEngine engine = LifeEngine(args.height, args.width,
-        State::randb, Kernel::life, Activation::life);
+    Engine::InitRandom(args.seed);
+    printf("seed: %i\n", args.seed);
 
+    State state;
+    Kernel kernel;
+
+    Engine::InitState(&state, args.height, args.width, States::randb);
+    Engine::InitKernel(&kernel, Kernels::life);
 
     // MAIN PART
 
-    while(display.run())
+    while (display.run())
     {
         if (display.nextFrame())
         {
-            display.draw(engine.state);
-            engine.epoch();
+            display.draw(state.current);
+            Engine::Epoch(&state, &kernel, Activations::life);
         }
-
     }
+
+    Engine::DestroyState(&state);
+    Engine::DestroyKernel(&kernel);
 
     return 0;
 }
