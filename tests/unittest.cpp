d6d01d57fdc77951961d16300cbe2ba4e862fc03
Akira Kurusu <43274844+AkiraKurusuOnGitHub@users.noreply.github.com>
2023-04-19 21:46:48 +0200

    engine, activations

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -3,6 +3,8 @@
 
 #include "engine.h"
 
+#include <array>
+
 // https://github.com/doctest/doctest
 
 TEST_SUITE("RNG")
@@ -41,12 +43,19 @@ TEST_SUITE("STATES")
         CHECK(state.current != nullptr);
         CHECK(state.next != nullptr);
         CHECK(state.current[0] == 0.0f);
+
+        int MAX = state.height * state.width - 1;
+        state.current[MAX] = 2.0f;
+        CHECK(state.current[MAX] == 2.0f);
+
         Engine::DestroyState(&state);
         CHECK(state.height == 0);
         CHECK(state.width == 0);
         CHECK(state.current == nullptr);
         CHECK(state.next == nullptr);
     }
+
+    // test cases for pre-defined states
 }
 
 TEST_SUITE("KERNELS")
@@ -58,13 +67,71 @@ TEST_SUITE("KERNELS")
         CHECK(kernel.size != 0);
         CHECK(kernel.kernel != nullptr);
         CHECK(kernel.kernel[0] == 1.0f);
+
+        int MAX = kernel.size * kernel.size - 1;
+        kernel.kernel[MAX] = 2.0f;
+        CHECK(kernel.kernel[MAX] == 2.0f);
+
         Engine::DestroyKernel(&kernel);
         CHECK(kernel.size == 0);
         CHECK(kernel.kernel == nullptr);
+    }
+
+    // test cases for pre-defined kernels
+}
+
+TEST_SUITE("ACTIVATIONS")
+{
+    const int length = 7;
+    std::array<float, length> x = {0.0f, 0.5f, 1.0f, 3.1415f, 5.0f, 9.0f, 100.0f};
+
+    TEST_CASE("ACTIVATIONS - identity")
+    {
+        for (int i = 0; i < length; i++)
+        {
+            CHECK(Activations::identity(x[i]) == doctest::Approx(x[i]));
+        }
+    }
+
+    TEST_CASE("ACTIVATIONS - sigmoid")
+    {
+        std::array<float, length> y = {0.0f, 0.5f, 1.0f, 3.1415f, 5.0f, 9.0f, 100.0f};
+
+        for (int i = 0; i < length; i++)
+        {
+            CHECK(Activations::identity(x[i]) == doctest::Approx(y[i]));
+        }
+    }
+
+    TEST_CASE("ACTIVATIONS - life")
+    {
     }
 }
 
 TEST_SUITE("ENGINE")
 {
-    // TODO
+    TEST_CASE("ENGINE - convolution (center)")
+    {
+        State state;
+        Kernel kernel;
+
+        Engine::InitState(&state, 3, 3, NULL);
+        Engine::InitKernel(&kernel, Kernels::full);
+
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
+
+        state.current[4] = 1.0f;
+
+        Engine::Epoch(&state, &kernel, Activations::identity);
+
+        for (int i = 0; i < 9; i++)
+        {
+            CHECK(state.current[i] == doctest::Approx(1.0));
+        }
+
+        Engine::DestroyState(&state);
+        Engine::DestroyKernel(&kernel);
+    }
 }
