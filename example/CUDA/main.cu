3d144bd5bf6002e45f88f44e042d44775aed3649
29th-Day <gerald.franschitz@gmail.com>
2023-08-25 17:14:48 +0200

    try-catch block

--- example/CUDA/main.cu
+++ example/CUDA/main.cu
@@ -10,44 +10,53 @@
  */
 
 #include <CellularAutomata>
+#include <iostream>
+
 
 using namespace CellularAutomata;
 
 // Custom activation function
-ACTIVATION(identity,
+ACTIVATION(customActivation,
 {
-    return x;
+    return (x > 10) ? 0: x;
 })
 
 int main(int argc, char* argv[])
 {
-    const int HEIGHT = 10;
-    const int WIDTH = 10;
+    const unsigned int HEIGHT = 10;
+    const unsigned int WIDTH = 10;
     const bool RECURSIVE = true;
     const unsigned int SEED = 0xAABBCC;
 
-    random::seed(SEED);
-
-    stateFunc<float> sf = nullptr;
-    kernelFunc<float> kf = Kernels::life;
-    // Activations::life<float> af;
-    identity<float> af;
-
-    State<float> stateGPU(HEIGHT, WIDTH, sf, Device::CUDA);
-    State<float> stateCPU(HEIGHT, WIDTH, sf, Device::CPU);
-    Kernel<float> kernel(3, kf, Device::CUDA);
-
-    States::Objects::Bipole1(&stateCPU, WIDTH / 2, HEIGHT / 2);
-
-    stateCPU.copyTo(&stateGPU);
-
-    for (int i = 0; i < 5; i++)
+    try
     {
-        stateGPU.copyTo(&stateCPU);
-        stateCPU.print();
-        std::cout << "-----" << std::endl;
+        random::seed(SEED);
 
-        CellularAutomata::Epoch(&stateGPU, &kernel, af, RECURSIVE);
+        stateFunc<float> sf = nullptr;
+        kernelFunc<float> kf = Kernels::life;
+        // Activations::life<float> af;
+        customActivation<float> af;
+
+        State<float> stateGPU(HEIGHT, WIDTH, sf, Device::CUDA);
+        State<float> stateCPU(HEIGHT, WIDTH, sf, Device::CPU);
+        Kernel<float> kernel(3, kf, Device::CUDA);
+
+        States::Objects::Glider(&stateCPU, WIDTH / 2, HEIGHT / 2);
+
+        stateCPU.copyTo(&stateGPU);
+
+        for (int i = 0; i < 5; i++)
+        {
+            stateGPU.copyTo(&stateCPU);
+            stateCPU.print();
+            std::cout << "-------------------" << std::endl;
+
+            CellularAutomata::Epoch(&stateGPU, &kernel, af, RECURSIVE);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Exception occured: " << e.what() << std::endl;
     }
 
     return 0;
